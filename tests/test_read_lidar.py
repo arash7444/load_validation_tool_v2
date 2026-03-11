9b7c551acddbbc5ba71fa8bc13be51e86b6a4060
Arash <arash.moh62@gmail.com>
2026-03-11 21:14:48 +0100

    Try to fix test function

--- tests/test_read_lidar.py
+++ tests/test_read_lidar.py
@@ -13,17 +13,18 @@ from load_validation_tool.data_readers import (
 import pandas as pd
 from pathlib import Path
 
-pth_lidar_base = Path("tests") / "lidar_data" # use it cross platform
+def test_read_lidar():
+    pth_lidar_base = Path("tests") / "lidar_data" # use it cross platform
 
-# pth_lidar_base = r"tests\lidar_data"
+    # pth_lidar_base = r"tests\lidar_data"
 
-start = pd.Timestamp("2020-05-01")
-end = pd.Timestamp("2020-05-03") + pd.Timedelta(days=1)
+    start = pd.Timestamp("2020-05-01")
+    end = pd.Timestamp("2020-05-03") + pd.Timedelta(days=1)
 
-lidar_files = lidar_finder(pth_lidar_base, start, end)
-if lidar_files is None:
-    raise Exception("No lidar files found.")
+    lidar_files = lidar_finder(pth_lidar_base, start, end)
+    if lidar_files is None:
+        raise Exception("No lidar files found.")
 
-lidar_avg, lidar_numeric, height_lidar, lidar_std = load_lidar_data(lidar_files[0])
-if len(lidar_avg) == 0:
-    raise Exception("Lidar Dataframe is empty.")
+    lidar_avg, lidar_numeric, height_lidar, lidar_std = load_lidar_data(lidar_files[0])
+    if len(lidar_avg) == 0:
+        raise Exception("Lidar Dataframe is empty.")
